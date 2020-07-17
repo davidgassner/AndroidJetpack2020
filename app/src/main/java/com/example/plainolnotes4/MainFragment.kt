ef2d14ba5a1285c1cb8dafbd4471ee7fcd853646
David Gassner <david.gassner@gmail.com>
2020-07-17 13:07:10 -0700

    02_01e

---
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.plainolnotes4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+}
