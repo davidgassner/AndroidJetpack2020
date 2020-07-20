b9ba7b93c29f349c07dfaae35a7485b467a8b54a
David Gassner <david.gassner@gmail.com>
2020-07-20 16:55:15 -0700

    04_01e

--- app/src/main/java/com/example/plainolnotes4/data/NoteEntity.kt
+++ app/src/main/java/com/example/plainolnotes4/data/NoteEntity.kt
@@ -1,9 +1,13 @@
 package com.example.plainolnotes4.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.plainolnotes4.NEW_NOTE_ID
 import java.util.*
 
+@Entity(tableName = "notes")
 data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
     var date: Date,
     var text: String
