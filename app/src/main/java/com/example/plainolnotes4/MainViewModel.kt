9557a0aa92cf2dcd59e5934b5c5cee43d870d553
David Gassner <david.gassner@gmail.com>
2020-07-20 10:29:12 -0700

    02_05e

--- app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.plainolnotes4
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.plainolnotes4.data.NoteEntity
+import com.example.plainolnotes4.data.SampleDataProvider
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val notesList = MutableLiveData<List<NoteEntity>>()
+
+    init {
+        notesList.value = SampleDataProvider.getNotes()
+    }
 }
---
+++ app/src/main/java/com/example/plainolnotes4/data/SampleDataProvider.kt
@@ -0,0 +1,27 @@
+package com.example.plainolnotes4.data
+
+import java.util.*
+
+class SampleDataProvider {
+
+    companion object {
+        private val sampleText1 = "A simple note"
+        private val sampleText2 = "A note a\nline feed"
+        private val sampleText3 = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus, magna eget ullamcorper eleifend, neque justo cursus nibh, sit amet fermentum nisi dui sed justo. Nulla ac nisl ut nulla euismod mollis. Aenean ullamcorper eu odio a rutrum. Mauris eu augue tortor. Pellentesque erat justo, gravida sed maximus eu, faucibus at justo. Aliquam ut nulla consectetur odio vestibulum vulputate vel id est. Aliquam erat volutpat.
+
+            Fusce maximus sagittis dolor in tempor. Duis vehicula congue lectus eu lobortis. Integer placerat fermentum sapien, vel feugiat sapien pellentesque non. Integer nec nibh sit amet ex lacinia pretium sed et eros. Nam id consequat erat, eleifend mollis quam. In dictum lobortis quam vel tincidunt. Vestibulum non lobortis neque. Phasellus pharetra malesuada mauris eget blandit. Sed ornare nisl id nisl tristique placerat.
+        """.trimIndent()
+
+        private fun getDate(diff: Long): Date {
+            return Date(Date().time + diff)
+        }
+
+        fun getNotes() = arrayListOf(
+            NoteEntity(1, getDate(0), sampleText1),
+            NoteEntity(2, getDate(1), sampleText2),
+            NoteEntity(3, getDate(2), sampleText3)
+        )
+
+    }
+}
