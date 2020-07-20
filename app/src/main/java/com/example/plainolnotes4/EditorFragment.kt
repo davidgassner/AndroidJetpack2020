cd60144e5b87f36080704e2471028ea69faf5c52
David Gassner <david.gassner@gmail.com>
2020-07-20 12:28:38 -0700

    03_01e

---
+++ app/src/main/java/com/example/plainolnotes4/EditorFragment.kt
@@ -0,0 +1,26 @@
+package com.example.plainolnotes4
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditorFragment : Fragment() {
+
+    private lateinit var viewModel: EditorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.editor_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/plainolnotes4/EditorViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.plainolnotes4
+
+import androidx.lifecycle.ViewModel
+
+class EditorViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
