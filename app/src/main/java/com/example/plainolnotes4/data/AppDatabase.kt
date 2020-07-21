fdb8f7d71ac5a664369289bd427d3e83062d69e5
David Gassner <david.gassner@gmail.com>
2020-07-21 15:03:27 -0700

    04_04e

---
+++ app/src/main/java/com/example/plainolnotes4/data/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.plainolnotes4.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase: RoomDatabase() {
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "plainolnotes.db"
+                    ).build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
