d8e3f5a5502929b22db804ff5024812da4f8e6a3
David Gassner <david.gassner@gmail.com>
2020-07-22 13:44:00 -0700

    04_02e

---
+++ app/src/main/java/com/example/plainolnotes4/data/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.plainolnotes4.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: NoteEntity)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(notes: List<NoteEntity>)
+
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAll(): LiveData<List<NoteEntity>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNoteById(id: Int): NoteEntity?
+
+}
