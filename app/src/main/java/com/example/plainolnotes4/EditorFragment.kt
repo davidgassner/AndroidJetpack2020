671d6bd4ff80addd2e0acb12018ce89f94581c85
David Gassner <david.gassner@gmail.com>
2020-07-24 15:54:03 -0700

    06_01e

--- app/src/main/java/com/example/plainolnotes4/EditorFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/EditorFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.plainolnotes4.databinding.EditorFragmentBinding
@@ -32,8 +33,10 @@ class EditorFragment : Fragment() {
         }
         setHasOptionsMenu(true)
 
+        viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
+
         binding = EditorFragmentBinding.inflate(inflater, container, false)
-        binding.editor.setText("You selected note number ${args.noteId}")
+        binding.editor.setText("")
 
         requireActivity().onBackPressedDispatcher.addCallback(
             viewLifecycleOwner,
@@ -43,6 +46,11 @@ class EditorFragment : Fragment() {
                 }
             }
         )
+
+        viewModel.currentNote.observe(viewLifecycleOwner, Observer {
+            binding.editor.setText(it.text)
+        })
+        viewModel.getNoteById(args.noteId)
 
         return binding.root
     }
@@ -57,11 +65,6 @@ class EditorFragment : Fragment() {
     private fun saveAndReturn(): Boolean {
         findNavController().navigateUp()
         return true
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
     }
 
 }
--- app/src/main/java/com/example/plainolnotes4/EditorViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/EditorViewModel.kt
@@ -1,7 +1,32 @@
 package com.example.plainolnotes4
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.plainolnotes4.data.AppDatabase
+import com.example.plainolnotes4.data.NoteEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class EditorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class EditorViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val database = AppDatabase.getInstance(app)
+    val currentNote = MutableLiveData<NoteEntity>()
+
+    fun getNoteById(noteId: Int) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val note =
+                    if (noteId != NEW_NOTE_ID) {
+                        database?.noteDao()?.getNoteById(noteId)
+                    } else {
+                        NoteEntity()
+                    }
+                currentNote.postValue(note)
+            }
+        }
+    }
+
 }
