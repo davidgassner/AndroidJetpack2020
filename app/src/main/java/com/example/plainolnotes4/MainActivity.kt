1ba066174d41b390ce44f44c84708e037f08046f
Larry Hunter <larry.hunter@outlook.com>
2021-03-17 09:59:17 -0600

    * Updated the application name shown by adding a label to the MainActivity.kt * Added the icon image for the app.

--- app/src/main/java/com/example/plainolnotes4/MainActivity.kt
+++ app/src/main/java/com/example/plainolnotes4/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.plainolnotes4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
